134cf5a1c18600e2766036f2ba078cc589afc3ec
FilipeCampos2424 <filipegames2343@gmail.com>
2026-04-14 08:27:14 -0300

    Resolvido: (Sistema Financeiro) Não aparecia, paginas atualizadas

--- includes/menu.php
+++ includes/menu.php
@@ -3,7 +3,7 @@ $perfil = $_SESSION["perfil"];
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
 <div class="container">
-<p class="">Sistema Financeiro</p>
+<a class= "titulo">Sistema Financeiro</a>
 <ul class="navbar-nav ms-auto">
 <li class="nav-item">
 <a class="nav-link" href="dashboard.php">Dashboard</a>
